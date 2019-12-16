c7d7c585de8fff381b0cd61c55d2f1d8f5af2c64
Katsuki Shuto <stk2k@sazysoft.com>
2019-12-16 14:47:50 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

---
+++ src/Package/KnotHttpPackage.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace KnotPhp\Module\KnotConsole\Package;
+
+use KnotLib\Kernel\Module\PackageInterface;
+use KnotPhp\Module\KnotHttp\KnotHttpResponderModule;
+use KnotPhp\Module\KnotHttp\KnotHttpRoutingMiddlewareModule;
+
+class KnotHttpPackage implements PackageInterface
+{
+    /**
+     * Get package module list
+     *
+     * @return string[]
+     */
+    public static function getModuleList() : array
+    {
+        return [
+            KnotHttpResponderModule::class,
+            KnotHttpRoutingMiddlewareModule::class,
+        ];
+    }
+}
